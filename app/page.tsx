1aec57a41198a7b40dcce779db2b4a32a4527ebd
Jose <jose@oldcutler.club>
2023-11-19 23:41:57 -0500

    removed spaces in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,7 @@ export default function Home() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
            }}
           />
-
         </div>
-
         <div className="py-2 sm:py-20 lg:pb-40">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
@@ -58,12 +56,8 @@ export default function Home() {
                     height={1442}
                     className="rounded-md shadow-2xl ring-1 ring-grayu-900/10"
                   />
-
-
                 </div>
-
               </div>
-
             </div>
 
           </div>
@@ -78,12 +72,7 @@ export default function Home() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
            }}
           />
-
         </div>
-
-
-
-
         </div>
     </main>
   )
